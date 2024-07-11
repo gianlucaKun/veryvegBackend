4f2e3fe5fbc9861642150f4118fe64e4793747f8
gianlucaKun <gianluca.andreoni95@gmail.com>
2024-07-11 12:18:16 +0200

    USER - controller e service

--- src/main/java/com/veryveg/product/controller/UserController.java
+++ src/main/java/com/veryveg/product/controller/UserController.java
@@ -1,5 +1,24 @@
 package com.veryveg.product.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.veryveg.product.DTO.UserDTO;
+import com.veryveg.product.entity.User;
+import com.veryveg.product.service.UserService;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
+	@Autowired
+	private UserService uService;
+	
+	@PostMapping("/create")
+	public UserDTO createUser (@RequestBody User u) {
+		return uService.createUserService(u);
+	}
 }
--- src/main/java/com/veryveg/product/service/UserService.java
+++ src/main/java/com/veryveg/product/service/UserService.java
@@ -1,8 +1,13 @@
 package com.veryveg.product.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.veryveg.product.DTO.UserDTO;
+import com.veryveg.product.entity.User;
+import com.veryveg.product.mapper.UserMapper;
 import com.veryveg.product.repository.UserRepository;
 
 @Service
@@ -10,6 +15,16 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepo;
+	@Autowired
+	private UserMapper userMap;
 	
-	
+	public UserDTO createUserService (User u) {
+		Optional<User> finded = userRepo.findById(u.getId());
+		
+		if (finded == null) {
+			User saved = userRepo.save(u);
+			return userMap.toUserDTO(saved);
+		}
+		return null;
+	}
 }
